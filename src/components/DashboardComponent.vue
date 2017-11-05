1164983463e0147c70dfae6d694322e2f65edab6
sisinduku <saptanto.sindu@gmail.com>
2017-11-05 08:48:49 +0700

    Add Trees Dashboard

---
+++ src/components/DashboardComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <h1>Mango Tree</h1>
+        {{mangoTree}}
+      </b-col>
+    </b-row>
+    <b-card-group columns>
+      <b-card v-for="tree in mangoTree" :key="tree['.key']" :header="tree.treeName">
+        <p class="card-text text-left">
+          Age: {{tree.age}}
+        </p>
+        <p class="card-text text-left">
+          Height: {{tree.height}}
+        </p>
+        <p class="card-text text-left">
+          Capacity: {{tree.capacity}}
+        </p>
+        <p class="card-text text-left">
+          Fruits: {{Object.keys(tree.fruits).length}}
+        </p>
+        <p class="card-text text-left">
+          Harvested: {{tree.harvested}}
+        </p>
+        <p class="card-text text-left">
+          Health Status: {{tree.isHealthy}}
+        </p>
+      </b-card>
+    </b-card-group>
+  </b-container>
+</template>
+<script>
+import db from '../assets/js/firebase.conf'
+const mangoRef = db.ref('trees')
+
+export default {
+  name: 'DashboardComponent',
+
+  data: () => ({}),
+
+  firebase: {
+    mangoTree: mangoRef
+  }
+}
+</script>
+<style scoped>
+</style>
