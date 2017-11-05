52ff74efbb91058350341435c8d0d04238faf210
sisinduku <saptanto.sindu@gmail.com>
2017-11-05 11:22:55 +0700

    Integrating Firebase result with Dashboard

--- src/components/DashboardComponent.vue
+++ src/components/DashboardComponent.vue
@@ -3,31 +3,40 @@
     <b-row>
       <b-col>
         <h1>Mango Tree</h1>
-        {{mangoTree}}
       </b-col>
     </b-row>
-    <b-card-group columns>
-      <b-card v-for="tree in mangoTree" :key="tree['.key']" :header="tree.treeName">
-        <p class="card-text text-left">
-          Age: {{tree.age}}
-        </p>
-        <p class="card-text text-left">
-          Height: {{tree.height}}
-        </p>
-        <p class="card-text text-left">
-          Capacity: {{tree.capacity}}
-        </p>
-        <p class="card-text text-left">
-          Fruits: {{Object.keys(tree.fruits).length}}
-        </p>
-        <p class="card-text text-left">
-          Harvested: {{tree.harvested}}
-        </p>
-        <p class="card-text text-left">
-          Health Status: {{tree.isHealthy}}
-        </p>
-      </b-card>
-    </b-card-group>
+    <b-row>
+      <b-col>
+        <b-card-group deck>
+          <b-card v-for="tree in mangoTree" :key="tree['.key']" :header="tree.treeName" :header-bg-variant="(tree._isHealthy) ? 'primary' : 'danger'">
+            <p class="card-text text-left">
+              Age: {{tree._age}}
+            </p>
+            <p class="card-text text-left">
+              Height: {{tree._height}} Cm
+            </p>
+            <p class="card-text text-left">
+              Last Capacity: {{tree._capacity}} Fruits
+            </p>
+            <p class="card-text text-left">
+              Fruits:
+              <ul class="inline-block">
+                <li v-for="(fruit, index) in tree._fruits" :key="index">
+                  Fruit: {{fruit.fruitName}}
+                  Quality: {{fruit.quality}}
+                </li>
+              </ul>
+            </p>
+            <p class="card-text text-left">
+              Last Harvested: {{tree._harvested}}
+            </p>
+            <p class="card-text text-left">
+              Health Status: {{(tree._isHealthy) ? 'Healthy' : 'Dead'}}
+            </p>
+          </b-card>
+        </b-card-group>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 <script>
@@ -37,7 +46,9 @@ const mangoRef = db.ref('trees')
 export default {
   name: 'DashboardComponent',
 
-  data: () => ({}),
+  data: () => ({
+    mangoTree: ''
+  }),
 
   firebase: {
     mangoTree: mangoRef
